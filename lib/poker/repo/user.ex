56ab534eb1c460617b33516cf9c018fc26fb5bcc
Alex <a.ablayev@gmail.com>
2023-05-25 15:28:58 +0300

    fix user session timeout; format

--- lib/poker/repo/user.ex
+++ lib/poker/repo/user.ex
@@ -1,7 +1,8 @@
 defmodule Poker.Repo.User do
   use GenServer
 
-  @user_session_timeout 1 # minutes
+  # minutes
+  @user_session_timeout 60
 
   # Client
   def start_link(_), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
@@ -19,23 +20,23 @@ defmodule Poker.Repo.User do
   def init(opts), do: {:ok, opts}
 
   @impl true
-  def handle_call({:insert, user}, _from, state), do:
-    {:reply, :ok, Map.put(state, user.id, user)}
+  def handle_call({:insert, user}, _from, state), do: {:reply, :ok, Map.put(state, user.id, user)}
 
   @impl true
-  def handle_call({:get, id}, _from, state), do:
-    {:reply, Map.get(state, id), state}
+  def handle_call({:get, id}, _from, state), do: {:reply, Map.get(state, id), state}
 
   @impl true
-  def handle_call(:all, _from, state), do:
-    {:reply, Map.values(state), state}
+  def handle_call(:all, _from, state), do: {:reply, Map.values(state), state}
 
   @impl true
   def handle_cast(:cleanup, state) do
     drop_ids =
       Map.values(state)
-      |> Enum.filter(fn x -> Timex.diff(Timex.now("UTC"), x.created_at, :minutes) >= @user_session_timeout end)
+      |> Enum.filter(fn x ->
+        Timex.diff(Timex.now("UTC"), x.created_at, :minutes) >= @user_session_timeout
+      end)
       |> Enum.map(fn x -> x.id end)
+
     {:noreply, Map.drop(state, drop_ids)}
   end
 end
