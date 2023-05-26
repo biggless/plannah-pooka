2ff22d2d035530a883db9483026e62dee48eab1e
Alex <a.ablayev@gmail.com>
2023-05-26 13:20:35 +0300

    review ux

--- lib/poker/repo/room.ex
+++ lib/poker/repo/room.ex
@@ -8,6 +8,8 @@ defmodule Poker.Repo.Room do
 
   def all(), do: GenServer.call(__MODULE__, :all)
 
+  def get(id), do: GenServer.call(__MODULE__, {:get, id})
+
   def get_by_user_id(user_id),
     do:
       all()
@@ -20,13 +22,14 @@ defmodule Poker.Repo.Room do
   def init(opts), do: {:ok, opts}
 
   @impl true
-  def handle_call({:insert, room}, _from, state),
-    do: {:reply, room, Map.put(state, room.id, room)}
+  def handle_call({:insert, room}, _from, state), do: {:reply, room, Map.put(state, room.id, room)}
+
+  @impl true
+  def handle_call({:get, id}, _from, state), do: {:reply, Map.get(state, id), state}
 
   @impl true
   def handle_call(:all, _from, state), do: {:reply, Map.values(state), state}
 
   @impl true
-  def handle_call({:update, room}, _from, state),
-    do: {:reply, room, Map.put(state, room.id, room)}
+  def handle_call({:update, room}, _from, state), do: {:reply, room, Map.put(state, room.id, room)}
 end
--- lib/poker_web/live/room_live.ex
+++ lib/poker_web/live/room_live.ex
@@ -5,7 +5,7 @@ defmodule PokerWeb.RoomLive do
     with %{"user_id" => user_id} <- session,
          user when user != nil <- Poker.Repo.User.get(user_id),
          room <- Poker.Repo.Room.get_by_user_id(user.id) do
-      {:ok, assign(socket, user: user, room: room)}
+      {:ok, assign(socket, user: user, roomcode: "", room: room)}
     else
       _ -> {:ok, push_redirect(socket, to: ~p"/auth")}
     end
@@ -14,20 +14,32 @@ defmodule PokerWeb.RoomLive do
   def render(%{room: nil} = assigns) do
     ~H"""
     <div class="flex justify-center items-center min-h-screen bg-gray-100">
-      <div class="bg-white p-8 rounded shadow-md flex">
-        <div class="w-1/2 pr-4">
-          <button
-            type="button"
-            class="w-full bg-indigo-500 text-white font-bold py-2 px-4 rounded focus:outline-none hover:bg-indigo-600"
-            phx-click="new-room"
-          >
-            Create room
-          </button>
-        </div>
-        <div class="w-1/2 pl-4 flex flex-col justify-center">
-          <p>Join</p>
-          <p>Room</p>
-        </div>
+      <div class="bg-white p-8 rounded shadow-md">
+        <h2 class="text-2xl font-semibold mb-6">Кімната</h2>
+        <.form
+          action={~p"/auth"}
+          phx-submit="join-room"
+        >
+          <div class="mb-4">
+            <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Код кімнати</label>
+            <input
+              type="text"
+              id="roomcode"
+              class="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+              name="roomcode"
+              value={@roomcode}
+              autofocus
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              class="w-full bg-indigo-500 text-white font-bold py-2 px-4 rounded focus:outline-none hover:bg-indigo-600"
+            >
+              До кімнати
+            </button>
+          </div>
+        </.form>
       </div>
     </div>
     """
@@ -39,16 +51,18 @@ defmodule PokerWeb.RoomLive do
     """
   end
 
-  def handle_event("new-room", _params, socket) do
+  def handle_event("join-room", %{"roomcode" => roomcode}, socket) do
     user = socket.assigns.user
-    room = Poker.Model.Room.create(user.id)
-    join_room(user, room)
-    {:noreply, assign(socket, user: user, room: room)}
-  end
 
-  defp join_room(user, room),
-    do:
-      room
-      |> Poker.Model.Room.add_user(user)
-      |> Poker.Repo.Room.update()
+    room = case Poker.Repo.Room.get(roomcode) do
+      nil -> Poker.Model.Room.create(user.id)
+      %Poker.Model.Room{} = room -> room
+    end
+
+    room
+    |> Poker.Model.Room.add_user(user)
+    |> Poker.Repo.Room.update()
+
+    {:noreply, assign(socket, room: room)}
+  end
 end
